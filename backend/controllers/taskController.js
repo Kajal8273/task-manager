439d196fb0d97cf7cac212ee208b710ef63e9429
sagar kumar <sagarkumsrdotcom3@gmail.com>
2024-07-07 01:19:53 +0530

    Initial commit

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,75 @@
+const Task = require('../models/Task');
+
+// Get all tasks
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get a single task
+exports.getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (task) {
+      res.json(task);
+    } else {
+      res.status(404).json({ message: 'Task not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Create a new task
+exports.createTask = async (req, res) => {
+  const { title, description, dueDate } = req.body;
+  const task = new Task({
+    title,
+    description,
+    dueDate
+  });
+  try {
+    const newTask = await task.save();
+    res.status(201).json(newTask);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Update a task
+exports.updateTask = async (req, res) => {
+  const { title, description, dueDate } = req.body;
+  try {
+    const task = await Task.findById(req.params.id);
+    if (task) {
+      task.title = title || task.title;
+      task.description = description || task.description;
+      task.dueDate = dueDate || task.dueDate;
+      const updatedTask = await task.save();
+      res.json(updatedTask);
+    } else {
+      res.status(404).json({ message: 'Task not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Delete a task
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (task) {
+      await task.remove();
+      res.json({ message: 'Task removed' });
+    } else {
+      res.status(404).json({ message: 'Task not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
